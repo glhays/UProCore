f64d9afbb7fc6ffd60b65efa5540d039094faed6
Greg Hays <glhays.dev@gmail.com>
2024-06-25 16:42:47 -0700

    INFRA: Initialize Infrastructure Project
    Closes #5

---
+++ UPro.Core.Infrastructure/Program.cs
@@ -0,0 +1,18 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// This source code is subject to terms and conditions set forth by UProCore
+// ----------------------------------------------------------------------------------
+
+using UPro.Core.Infrastructure.Services;
+
+namespace UPro.Core.Infrastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scriptGenerationService = new ScriptGenerationService();
+            scriptGenerationService.GenerateBuildScript();
+        }
+    }
+}

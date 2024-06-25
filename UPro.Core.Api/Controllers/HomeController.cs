9bc6e5368fbfd66db4943bf3f432331447a99efa
Greg Hays <glhays.dev@gmail.com>
2024-06-25 14:52:27 -0700

    INFRA: Initialize Core Api Project
    Closes #3

---
+++ UPro.Core.Api/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// This source code is subject to terms and conditions set forth by UProCore
+// ----------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace UPro.Core.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Welcome to UPro.Core.Api");
+        }
+    }
+}

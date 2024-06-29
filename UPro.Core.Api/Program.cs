82795e03476bb1f5c3fd03a882e96faf00f14a22
Greg Hays <glhays.dev@gmail.com>
2024-06-29 13:12:50 -0700

    BROKERS: Logging LogDebug
    Closes #15

--- UPro.Core.Api/Program.cs
+++ UPro.Core.Api/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using UPro.Core.Api.Brokers.DateTimes;
+using UPro.Core.Api.Brokers.Loggings;
 using UPro.Core.Api.Brokers.Storages;
 
 namespace UPro.Core.Api
@@ -25,6 +26,10 @@ namespace UPro.Core.Api
             builder.Services.AddTransient<
                 IStorageBroker,
                 StorageBroker>();
+
+            builder.Services.AddTransient<
+                ILoggingBroker,
+                LoggingBroker>();
 
             builder.Services.AddTransient<
                 IDateTimeBroker,

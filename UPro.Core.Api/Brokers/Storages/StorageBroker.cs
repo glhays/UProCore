ef352a4c790614812c6ea5c052bb5061fecc6c18
Greg Hays <glhays.dev@gmail.com>
2024-07-12 17:24:06 -0700

    Brokers: CRUD Insert Method

--- UPro.Core.Api/Brokers/Storages/StorageBroker.cs
+++ UPro.Core.Api/Brokers/Storages/StorageBroker.cs
@@ -3,6 +3,8 @@
 // This source code is subject to terms and conditions set forth by UProCore
 // ----------------------------------------------------------------------------------
 
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using EFxceptions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -26,6 +28,20 @@ namespace UPro.Core.Api.Brokers.Storages
                 this.configuration.GetConnectionString(name: "UProDbConnection");
 
             optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        private async ValueTask<T> InsertAsync<T>(T entity)
+        {
+            var broker = new StorageBroker(this.configuration);
+            broker.Entry(entity).State = EntityState.Added;
+            await broker.SaveChangesAsync();
+
+            return entity;
+        }
+
+        private void DetachSavedEntity<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Detached;
         }
     }
 }
